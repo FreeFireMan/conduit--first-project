a6169b0dac205ab3660b9ae0de24315c6dcabcbd
Fedenko Maksym <feden2906@gmail.com>
2021-01-02 22:37:01 +0200

    have problems with fetch

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,6 +1,12 @@
 import './Posts.css'
+import doFetch from "../../services/doFetch";
+import {useEffect} from "react/cjs/react.production.min";
 
 export default function Posts (){
+  // useEffect( () => {
+  doFetch('/api/articles')
+      // .then(value => console.log(value))
+  // }, [])
     return (
         <div>
             111111
