f90b3db7b9e7b589557df270e9870b9b30d879ef
Fedenko Maksym <feden2906@gmail.com>
2021-01-04 23:30:31 +0200

    create full component

--- src/components/post-full/PostFull.js
+++ src/components/post-full/PostFull.js
@@ -1,13 +1,10 @@
 import './PostFull.css'
-import {
-  BrowserRouter as Router, Switch, Route, Link, withRouter
-} from "react-router-dom";
 
-export default function PostFull (props){
+export default function PostFull(props) {
   console.log(props);
   return (
-        <div>
-            lol
-        </div>
-    );
+      <div>
+        lol
+      </div>
+  );
 }
