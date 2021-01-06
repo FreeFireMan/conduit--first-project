19cb9c0a2e38f0eb25bdc0c1e2e3e79fcdf8c431
Fedenko Maksym <feden2906@gmail.com>
2021-01-07 00:40:51 +0200

    create user profile page

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -3,14 +3,18 @@ import FooterPage from "../footer-page/FooterPage";
 import {Link} from "react-router-dom";
 import {showFeed} from "../../redux/action-creators";
 import React from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 export default function UserProfile (){
 
-const dispatch = useDispatch
+const dispatch = useDispatch()
+  const active = useSelector(({homePage: {active}}) => active)
+  const loggedIn = useSelector(({user: {loggedIn}}) => loggedIn)
 
-    return (
+
+  return (
         <div>
+
             <header className='profile-header'>
               <img className='profile-photo' src="https://icdn.lenta.ru/images/2020/09/11/13/20200911130707980/pwa_list_sq_1280_b52f104ad4178fa38bd74039fba7d4b2.png" alt="foto"/>
               {/*TODO*/}
@@ -19,22 +23,19 @@ const dispatch = useDispatch
               <div className='profile-btn-settings-wrapper'>
                   <Link to={'/settings'} className='profile-btn-settings'>Edit Profile Settings</Link>
               </div>
-
             </header>
 
             <body className='profile-body'>
             <div className='profile-nav-link'>
-              <Link to={loggedIn ? '/' : '/login'}
-                    onClick={() => dispatch(showFeed('your'))}
-                    className={active === 'your' ? 'nItem chosenItem' : 'nItem'}>Your Feed</Link>
               <div onClick={() => dispatch(showFeed('global'))}
-                   className={active === 'global' ? 'nItem chosenItem' : 'nItem'}>Global Feed
+                   className={active === 'global' ? 'nItem chosenItem' : 'nItem'}>My Posts
               </div>
-              <div onClick={() => dispatch(showFeed('chosen'))}
-                   className={active === 'chosen' ? 'nItem chosenItem' : 'nItem'}>#tag
-              </div>
+              <Link to={loggedIn || '/login'}
+                    onClick={() => dispatch(showFeed('your'))}
+                    className={active === 'your' ? 'nItem chosenItem' : 'nItem'}>Favorited Posts</Link>
             </div>
             </body>
+
 
             <FooterPage/>
         </div>
