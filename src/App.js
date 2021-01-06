e983b7eb6ca57704a0a2038023e741b5057df6a7
freefireman <beluh.c.s@gmail.com>
2021-01-06 14:41:06 +0200

    some upgrade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
-import {Route, Switch, withRouter} from "react-router-dom";
 import Header from "./components/header/Header";
-import Home from "./components/home/Home";
-import SignIn from "./components/sign-in/SignIn";
-import SignUp from "./components/sign-up/SignUp";
-import PostFull from "./components/post-full/PostFull";
+import Routers from "./routers"
 
 function App() {
 
   return (
       <div className="App">
         <Header/>
-
-        <Switch>
-          <Route path={`/article/:linkToFullPost`} component={PostFull}/>
-          <Route path={'/login'} component={SignIn}/>
-          <Route path={'/register'} component={SignUp}/>
-          <Route path={'/'} component={Home}/>
-        </Switch>
+        <Routers/>
       </div>
   );
 }
