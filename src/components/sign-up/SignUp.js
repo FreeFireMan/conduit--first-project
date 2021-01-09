c0783522fb2168eccc5a16f894a784484a8def64
Fedenko Maksym <feden2906@gmail.com>
2021-01-09 22:36:46 +0200

    add error component

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -2,6 +2,7 @@ import {Link} from "react-router-dom";
 import FooterPage from "../footer-page/FooterPage";
 import {useState} from "react";
 import postFetch from "../../services/postFetch";
+import Error from "../error/Error";
 
 export default function SignUp() {
 
@@ -23,6 +24,9 @@ export default function SignUp() {
 
             <p className='sign-title'>Sign up</p>
             <Link className='sign-link' to='/login'>Have an account?</Link>
+            <div className='errors-wrapper'>
+              <Error/>
+            </div>
             <input onChange={(e) => setUsername(e.currentTarget.value)}
                    className='sign-input' type='text' placeholder='Username'/>
             <input onChange={(e) => setEmail(e.currentTarget.value)}
