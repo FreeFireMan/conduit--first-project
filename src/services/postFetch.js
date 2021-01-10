7c91047de51aea508856c7f48bc03232381d4f70
Fedenko Maksym <feden2906@gmail.com>
2021-01-10 16:33:06 +0200

    for Yura

--- src/services/postFetch.js
+++ src/services/postFetch.js
@@ -1,5 +1,7 @@
 export default function postFetch (url, data){
   const baseUrl = 'https://conduit.productionready.io'
-  return fetch(baseUrl + url, {method: 'POST', body: JSON.stringify(data)})
+
+  return fetch(baseUrl + url,
+      {data: JSON.stringify(data), method: 'POST', headers: {authorization: ""}})
             .then(response => response.json())
 }
