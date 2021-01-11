7dbd082fe34b0dfeb8d5b20ebc8e414d4cd5e154
Fedenko Maksym <feden2906@gmail.com>
2021-01-11 14:28:02 +0200

    for Yura

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -4,9 +4,9 @@ export default function Error ({errors, errors: {email, password, username}}){
   const errorKey = Object.keys(errors)
     return (
             <ul>
-              {errorKey.map((key, i) => {
-                errors.find()
-              })}
+              {/*{errorKey.map((key, i) => {*/}
+              {/*  errors.find()*/}
+              {/*})}*/}
               {email && email.map((value, i) => <li key={i} className='errors'><b>email {value}</b></li>)}
               {password && password.map((value, i) => <li key={i} className='errors'><b>password {value}</b></li>)}
               {username && username.map((value, i) => <li key={i} className='errors'><b>username {value}</b></li>)}
