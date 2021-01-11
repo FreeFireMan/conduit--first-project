df1d2725ae77fc9bf4a9059a6eced9548239f6ea
Fedenko Maksym <feden2906@gmail.com>
2021-01-11 23:54:34 +0200

    have problem with take token from LS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
         <Router>
+          {/* TODO - add wrapper component to App */}
           <App/>
         </Router>
       </Provider>
